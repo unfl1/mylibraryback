7b1a716b555837634892561a48763ef6a71e7f62
unfl1 <flxmaut10@gmail.com>
2024-06-07 05:45:30 +0900

    수정1

--- src/test/java/com/example/mylibrary/mylibary/MylibaryApplicationTests.java
+++ src/test/java/com/example/mylibrary/mylibary/MylibaryApplicationTests.java
@@ -1,9 +1,8 @@
 package com.example.mylibrary.mylibary;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class MylibaryApplicationTests {
 
 	@Test
