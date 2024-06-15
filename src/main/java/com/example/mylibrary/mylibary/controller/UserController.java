fb2e56a72537259bc03d39a9086bc1e53f11e753
unfl1 <flxmaut10@gmail.com>
2024-06-15 22:43:33 +0900

    이유짐작1

--- src/main/java/com/example/mylibrary/mylibary/controller/UserController.java
+++ src/main/java/com/example/mylibrary/mylibary/controller/UserController.java
@@ -10,13 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -25,14 +23,14 @@ public class UserController {
     private final UserService userService;
     private final UserSecurityService userSecurityService;
 
-    @PostMapping("/user/signup")
+    @PostMapping("/api/user/signup")
     public ResponseEntity<Map<String, Object>> signup(@Valid @RequestBody Map<String, String> userCreateForm) {
         logger.info("Signup request received: {}", userCreateForm);
         Map<String, Object> resultMap = userService.createUser(userCreateForm);
         return new ResponseEntity<>(resultMap, resultMap.containsKey("error") ? HttpStatus.INTERNAL_SERVER_ERROR : HttpStatus.OK);
     }
 
-    @PostMapping("/user/login")
+    @PostMapping("/api/user/login")
     public ResponseEntity<Map<String, Object>> login(@RequestBody Map<String, String> loginRequest) {
         logger.info("Login request received: {}", loginRequest);
         Map<String, Object> resultMap = userSecurityService.authenticateUser(loginRequest);
