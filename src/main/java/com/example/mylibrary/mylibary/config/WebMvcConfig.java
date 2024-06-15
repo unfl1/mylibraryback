2a733c5a975195df45f2d6605d38bae12724e1db
unfl1 <flxmaut10@gmail.com>
2024-06-15 19:23:36 +0900

    전부 다 허용하게 설정

--- src/main/java/com/example/mylibrary/mylibary/config/WebMvcConfig.java
+++ src/main/java/com/example/mylibrary/mylibary/config/WebMvcConfig.java
@@ -11,8 +11,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // 모든 경로에 대해
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+        registry.addMapping("/**")
+                // Origin이 http://localhost:3000에 대해
+                .allowedOriginPatterns("*")
                 // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
